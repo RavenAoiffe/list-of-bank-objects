3129488a4257d6927345abf73c00cd137d754b9b
Ameyalli <ameyanezcmm@gmail.com>
2023-10-16 21:34:24 -0600

    Add key for Bank

---
+++ src/components/Bank.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Bank = ({bank}) => {
+
+    const {bankName,age,description} = bank;
+    return (
+        <div>
+            <div className="flex md:flex-row flex-col border items-center">
+                    <div className="w-32 p-3" >
+                        <img src={bank.url}/>
+                    </div>
+                    <ul className="m-2 py-1">
+                        <li className="p-2 ">
+                            <strong> Bank name:</strong> {bankName}</li>
+                        <li className="p-2"><strong>Age:</strong> {age}</li>
+                        <li className="p-2"><strong>Description:</strong> {description}</li>
+                    </ul>
+                </div>
+</div>
+    );
+}
+
+export default Bank;
--- src/components/BankList.jsx
+++ src/components/BankList.jsx
@@ -1,3 +1,5 @@
+import {generarId}  from '../../helpers';
+import Bank from './Bank';
 const BankList = ({ listOfBank }) => {
     return (
         <>
@@ -5,17 +7,10 @@ const BankList = ({ listOfBank }) => {
                 <div className="w-full bg-white shadow mt-5 table-auto transition-opacity">
                     <p className="bg-purple-600 text-white text-center py-3">Banks</p>
                     {listOfBank.map(bank => (
-                        <div className="flex md:flex-row flex-col border items-center">
-                            <div className="w-32 p-3" >
-                                <img src={bank.url}/>
-                            </div>
-                            <ul className="m-2 py-1">
-                                <li className="p-2 ">
-                                    <strong> Bank name:</strong> {bank.bankName}</li>
-                                <li className="p-2"><strong>Age:</strong> {bank.age}</li>
-                                <li className="p-2"><strong>Description:</strong> {bank.description}</li>
-                            </ul>
-                        </div>
+                        <Bank
+                            key={bank.id=generarId()}
+                            bank={bank}
+                        />
                     )
                     )}
 
